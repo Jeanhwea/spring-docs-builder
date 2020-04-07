8c90d545838d5a6e4e22d95dedce449b9effafa0
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-04-07 21:02:55 +0800

    fix

--- assets/build.sh
+++ assets/build.sh
@@ -3,7 +3,6 @@ set -e
 
 mv /app/entry.sh /usr/sbin/entry.sh &&
   chmod +x /usr/sbin/entry.sh &&
-  mkdir /app &&
   git clone https://github.com/spring-projects/spring-framework.git /app/spring &&
   cd /app/spring &&
   ./gradlew build asciidoctor
