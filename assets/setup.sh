835943b3fe9b07275217270d4404d0cf9d384e2a
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-01-03 17:00:07 -0600

    Update setup.sh

--- assets/setup.sh
+++ assets/setup.sh
@@ -11,7 +11,7 @@ git clone -q $SPRING_GIT_REPO /assets/source
 
 log "Building source code ..."
 cd /assets/source && chmod +x /assets/entrypoint.sh && \
-  ./gradlew build asciidoctor >/assets/gradle.log 2>&1
+  ./gradlew build asciidoctor
 
 log "Archiving build docs ..."
 ARCNAME=$(TZ='Asia/Shanghai' date +'%Y%m%d_%H%M%S_docs')
